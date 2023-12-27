7b21bbfe958f383bd05aff02889af412288db983
Savio Resende <savio@savioresende.com>
2023-12-27 02:23:14 -0600

    Changed the client to use server client.

--- src/Conveyor.php
+++ src/Conveyor.php
@@ -4,7 +4,7 @@ namespace Kanata\LaravelBroadcaster;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\PrivateChannel;
-use WebSocket\Client as WsClient;
+use Kanata\ConveyorServerClient\Client;
 
 class Conveyor
 {
@@ -16,28 +16,20 @@ class Conveyor
      */
     public function broadcast(array $channels, $event, array $payload = [])
     {
-        $url = config('conveyor.protocol', 'ws') . '://'
-            . config('conveyor.uri', '127.0.0.1') . ':'
-            . config('conveyor.port', 8002)
-            . (
-                !empty(config('conveyor.query')) ?
-                    '/?' . config('conveyor.query', '')
-                    : ''
-            );
-        $client = new WsClient($url);
-
         foreach ($channels as $channel) {
-            $client->text(json_encode([
-                'action' => 'channel-connect',
+            $options = [
+                'protocol' => config('conveyor.protocol', 'ws'),
+                'host' => config('conveyor.uri', '127.0.0.1'),
+                'port' => config('conveyor.port', 8002),
+                'query' => '?'.config('conveyor.query', ''),
                 'channel' => $channel->name,
-            ]));
-
-            $client->text(json_encode([
-                'action' => 'broadcast-action',
-                'data' => $payload['message'],
-            ]));
+                'timeout' => 1,
+                'onReadyCallback' => function(Client $currentClient) use ($payload) {
+                    $currentClient->send($payload['message']);
+                },
+            ];
+            $client = new Client($options);
+            $client->connect();
         }
-
-        $client->close();
     }
 }
