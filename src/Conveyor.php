318b33a4b4ba6f8df14a9bc3be4cef67cd794104
Savio Resende <savio@savioresende.com>
2023-12-27 03:37:36 -0600

    Added special condition for the unlimited token.2

--- src/Conveyor.php
+++ src/Conveyor.php
@@ -4,7 +4,10 @@ namespace Kanata\LaravelBroadcaster;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Support\Facades\Log;
 use Kanata\ConveyorServerClient\Client;
+use Exception;
+use WebSocket\TimeoutException;
 
 class Conveyor
 {
@@ -17,19 +20,30 @@ class Conveyor
     public function broadcast(array $channels, $event, array $payload = [])
     {
         foreach ($channels as $channel) {
-            $options = [
-                'protocol' => config('conveyor.protocol', 'ws'),
-                'host' => config('conveyor.uri', '127.0.0.1'),
-                'port' => config('conveyor.port', 8002),
-                'query' => '?'.config('conveyor.query', ''),
-                'channel' => $channel->name,
-                'timeout' => 1,
-                'onReadyCallback' => function(Client $currentClient) use ($payload) {
-                    $currentClient->send($payload['message']);
+            rescue(
+                callback: function () use ($channel, $payload) {
+                    $options = [
+                        'protocol' => config('conveyor.protocol', 'ws'),
+                        'host' => config('conveyor.uri', '127.0.0.1'),
+                        'port' => config('conveyor.port', 8002),
+                        'query' => '?'.config('conveyor.query', ''),
+                        'channel' => $channel->name,
+                        'timeout' => 1,
+                        'onReadyCallback' => function(Client $currentClient) use ($payload) {
+                            $currentClient->send($payload['message']);
+                        },
+                    ];
+                    $client = new Client($options);
+                    $client->connect();
                 },
-            ];
-            $client = new Client($options);
-            $client->connect();
+                rescue: function (Exception $e) {
+                    if ($e instanceof TimeoutException) {
+                        return;
+                    }
+                    Log::info('Conveyor failed to broadcast: ' . $e->getMessage());
+                },
+                report: false,
+            );
         }
     }
 }
