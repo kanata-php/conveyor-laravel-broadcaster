324fb2b5d9846303005627da228362fc9f0b2e12
Savio Resende <savio@savioresende.com>
2024-04-27 22:06:12 -0500

    Enhancement for cache usage for optimization - redis.

--- src/Conveyor.php
+++ src/Conveyor.php
@@ -2,13 +2,9 @@
 
 namespace Kanata\LaravelBroadcaster;
 
-use Conveyor\SubProtocols\Conveyor\Actions\BroadcastAction;
-use Conveyor\SubProtocols\Conveyor\Actions\ChannelConnectAction;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Support\Facades\Log;
-use Exception;
-use WebSocket\Client as WsClient;
+use Illuminate\Support\Facades\Cache;
 
 class Conveyor
 {
@@ -20,44 +16,16 @@ class Conveyor
      */
     public function broadcast(array $channels, $event, array $payload = []): void
     {
-        try {
-            foreach ($channels as $channel) {
-                $this->sendOneOff($channel, $payload['message']);
-            }
-        } catch (Exception $e) {
-            Log::error('Conveyor: Failed to broadcast to channel: ' . $channel, ['exception' => $e]);
-        }
-    }
-
-    protected function sendOneOff(string $channel, string $message)
-    {
-        $protocol = config('conveyor.protocol', 'ws');
-        $host = config('conveyor.uri', '127.0.0.1');
-        $port = config('conveyor.port', 8002);
-        $query = '?' . config('conveyor.query', '');
-        $uri = "{$protocol}://{$host}:{$port}/{$query}";
-
-        $client = new WsClient(
-            uri: $uri,
-            options: [
-                'timeout' => 3,
-            ],
-        );
-
-        // connect to channel
-        $channelConnectMessage = json_encode([
-            'action' => ChannelConnectAction::NAME,
-            'channel' => $channel,
-        ]);
-        $client->send($channelConnectMessage);
-
-        // broadcast
-        $client->send(json_encode([
-            'action' => BroadcastAction::NAME,
-            'data' => $message,
-        ]));
-        $client->receive();
-
-        $client->close();
+        Cache::lock('conveyor-messages', 2)
+            ->block(6, function () use ($channels, $event, $payload) {
+                foreach ($channels as $channel) {
+                    $messages = Cache::pull('conveyor-messages', []);
+                    array_push($messages, [
+                        'channel' => $channel->name,
+                        'message' => $payload['message'],
+                    ]);
+                    Cache::set('conveyor-messages', $messages, 10);
+                }
+            });
     }
 }
