7aaf79b083f3c699efe49025e0bec24dd3f9339d
Savio Resende <savio@savioresende.com>
2024-12-19 21:11:23 -0600

    Adjustment for the usage of this client outside of jacked server.

--- src/Conveyor.php
+++ src/Conveyor.php
@@ -16,8 +16,13 @@ class Conveyor
      */
     public function broadcast(array $channels, $event, array $payload = []): void
     {
+        $url = (config('conveyor.protocol') === 'ws' ? 'http' : 'https') . '://'
+            . config('conveyor.uri') . ':'
+            . config('conveyor.port') . '/conveyor/message'
+            . (config('conveyor.query') ? '?' . config('conveyor.query') : '');
+
         foreach ($channels as $channel) {
-            Http::post(config('app.url') . '/conveyor/message', [
+            Http::post($url, [
                 'channel' => $channel->name,
                 'message' => json_encode($payload),
             ]);
